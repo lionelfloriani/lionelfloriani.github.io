bb07ab5a2ee730da221c3877b2a10a61425e4450
lionelfloriani <lionel.floriani@outlook.com>
2022-12-08 20:56:29 +0100

    emoji animation + night mode

--- script.js
+++ script.js
@@ -69,3 +69,137 @@ function animate() {
 setTimeout(() => {
     animate();
 }, 500);
+
+var checkbox = document.querySelector("input");
+let redText = document.getElementById("r1");
+let redText1 = document.getElementById("r2");
+let redText2 = document.getElementById("r3");
+let redText3 = document.getElementById("r4");
+let redText4 = document.getElementById("r5");
+let redText5 = document.getElementById("r6");
+let redText6 = document.getElementById("r7");
+let main = document.getElementById("main");
+let blueText = document.getElementById("b1");
+let blueText1 = document.getElementById("b2");
+let blueText2 = document.getElementById("b3");
+let blueText3 = document.getElementById("b4");
+
+checkbox.addEventListener("change", function () {
+    if (this.checked) {
+        main.style.background = "#232946";
+        redText.style.color = "#eebbc3";
+        redText1.style.color = "#eebbc3";
+        redText2.style.color = "#eebbc3";
+        redText3.style.color = "#eebbc3";
+        redText4.style.color = "#eebbc3";
+        redText5.style.color = "#eebbc3";
+        redText6.style.color = "#eebbc3";
+        blueText.style.color = "#b8c1ec";
+        blueText1.style.color = "#b8c1ec";
+        blueText2.style.color = "#b8c1ec";
+        blueText3.style.color = "#b8c1ec";
+        redText.addEventListener("mouseover", function () {
+            redText.style.color = "#b8c1ec";
+        });
+        redText.addEventListener("mouseout", function () {
+            redText.style.color = "#eebbc3";
+        });
+        redText2.addEventListener("mouseover", function () {
+            redText2.style.color = "#b8c1ec";
+        });
+        redText2.addEventListener("mouseout", function () {
+            redText2.style.color = "#eebbc3";
+        });
+        redText3.addEventListener("mouseover", function () {
+            redText3.style.color = "#b8c1ec";
+        });
+        redText3.addEventListener("mouseout", function () {
+            redText3.style.color = "#eebbc3";
+        });
+        redText1.addEventListener("mouseover", function () {
+            redText1.style.color = "#b8c1ec";
+        });
+        redText1.addEventListener("mouseout", function () {
+            redText1.style.color = "#eebbc3";
+        });
+        redText6.addEventListener("mouseover", function () {
+            redText6.style.color = "#b8c1ec";
+        });
+        redText6.addEventListener("mouseout", function () {
+            redText6.style.color = "#eebbc3";
+        });
+    } else {
+        redText.style.color = "#e45858";
+        redText1.style.color = "#e45858";
+        redText2.style.color = "#e45858";
+        redText3.style.color = "#e45858";
+        redText4.style.color = "#e45858";
+        redText5.style.color = "#e45858";
+        redText6.style.color = "#e45858";
+        main.style.background = "#fffffe";
+        blueText.style.color = "#6246ea";
+        blueText1.style.color = "#6246ea";
+        blueText2.style.color = "#6246ea";
+        blueText3.style.color = "#6246ea";
+        redText.addEventListener("mouseover", function () {
+            redText.style.color = "#6246ea";
+        });
+        redText.addEventListener("mouseout", function () {
+            redText.style.color = "#e45858";
+        });
+        redText2.addEventListener("mouseover", function () {
+            redText2.style.color = "#6246ea";
+        });
+        redText2.addEventListener("mouseout", function () {
+            redText2.style.color = "#e45858";
+        });
+        redText3.addEventListener("mouseover", function () {
+            redText3.style.color = "#6246ea";
+        });
+        redText3.addEventListener("mouseout", function () {
+            redText3.style.color = "#e45858";
+        });
+        redText1.addEventListener("mouseover", function () {
+            redText1.style.color = "#6246ea";
+        });
+        redText1.addEventListener("mouseout", function () {
+            redText1.style.color = "#e45858";
+        });
+        redText6.addEventListener("mouseover", function () {
+            redText6.style.color = "#6246ea";
+        });
+        redText6.addEventListener("mouseout", function () {
+            redText6.style.color = "#e45858";
+        });
+    }
+});
+redText.addEventListener("mouseover", function () {
+    redText.style.color = "#6246ea";
+});
+redText.addEventListener("mouseout", function () {
+    redText.style.color = "#e45858";
+});
+redText2.addEventListener("mouseover", function () {
+    redText2.style.color = "#6246ea";
+});
+redText2.addEventListener("mouseout", function () {
+    redText2.style.color = "#e45858";
+});
+redText3.addEventListener("mouseover", function () {
+    redText3.style.color = "#6246ea";
+});
+redText3.addEventListener("mouseout", function () {
+    redText3.style.color = "#e45858";
+});
+redText1.addEventListener("mouseover", function () {
+    redText1.style.color = "#6246ea";
+});
+redText1.addEventListener("mouseout", function () {
+    redText1.style.color = "#e45858";
+});
+redText6.addEventListener("mouseover", function () {
+    redText6.style.color = "#6246ea";
+});
+redText6.addEventListener("mouseout", function () {
+    redText6.style.color = "#e45858";
+});
